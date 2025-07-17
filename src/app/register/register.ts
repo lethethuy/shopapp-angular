d9b7e38722eb5485c8d175b6f50ef0c4c7f28f3c
Lê Thế Thùy (KHN.SBU2) <thuylt1@kaopiz.com>
2025-07-17 17:47:13 +0700

    2025/07/17

--- src/app/register/register.ts
+++ src/app/register/register.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,6 +8,7 @@ import { Component } from '@angular/core';
   styleUrl: './register.scss'
 })
 export class Register {
+  @ViewChild('registerForm') registerForm!: NgForm;
   // Khai báo các biến tương ứng với các trường dữ liệu trong form
   phone: string;
   password: string;
@@ -22,7 +24,7 @@ export class Register {
       this.retypePassword = '';
       this.fullName = '';
       this.address = '';
-      this.isAccepted = false,
+      this.isAccepted = false;
       this.dateOfBirth = new Date();
       this.dateOfBirth.setFullYear(this.dateOfBirth.getFullYear()-18);
   }
@@ -39,6 +41,35 @@ export class Register {
       `isAccepted: ${this.isAccepted}`+
       `dateOfBirth: ${this.dateOfBirth}`
     alert(message)
+  }
+
+  // check password trung nhau
+  checkPasswordsMatch(){
+    if(this.password !== this.retypePassword){
+        this.registerForm.form.controls['retypePassword']
+        .setErrors({'passwordMismatch':true});
+    }else{
+        this.registerForm.form.controls['retypePassword'].setErrors(null);
+    }
+  }
+
+  // check age
+  checkAge(){
+    if(this.dateOfBirth){
+        const today = new Date();
+        const birthDate = new Date(this.dateOfBirth);
+        let age = today.getFullYear() - birthDate.getFullYear();
+        const monthDiff = today.getMonth() - birthDate.getMonth();
+        const dayDiff = today.getDay() - birthDate.getDay();
+        if(monthDiff<0 || (monthDiff===0 && dayDiff<0)){
+            age--;
+        }
+        if(age<18){
+            this.registerForm.form.controls['dateOfBirth'].setErrors({'invalidAge': true});
+        }else{
+            this.registerForm.form.controls['dateOfBirth'].setErrors(null);
+        }
+    }
   }
 
 }
